8b696d1f9e7c4e653aa5e05fc0ae53dd210f81f6
Pence <bunphengchhay@gmail.com>
2024-03-06 01:35:20 -0800

    update apiConfig and html title

--- client/src/config/apiConfig.js
+++ client/src/config/apiConfig.js
@@ -1,2 +1,9 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost/index.php';
-export default API_URL
+// Define the default base URL and API endpoint
+const DEFAULT_BASE_URL = 'http://54.67.117.128';
+const API_ENDPOINT = '/api/index.php';
+
+// Concatenate the base URL and API endpoint
+const BASE_URL = process.env.REACT_APP_API_URL || DEFAULT_BASE_URL;
+const API_URL = `${BASE_URL}${API_ENDPOINT}`;
+
+export default API_URL;
